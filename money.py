5810b05abe44994b70e28647fe22c9e0bf806f5d
Ionut Cotoi <ionut@devicehub.net>
2021-09-08 23:56:37 +0300

    Initial wealth model with functional viz

--- money.py
+++ money.py
@@ -3,8 +3,6 @@ from mesa import Agent, Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
-from mesa.visualization.modules import CanvasGrid, ChartModule
-from mesa.visualization.ModularVisualization import ModularServer
 
 
 def compute_gini(model):
@@ -58,39 +56,13 @@ class MoneyModel(Model):
             agent_reporters={'Wealth': 'wealth'}
         )
 
+        self.running = True
+        self.datacollector.collect(self)
+
     def step(self) -> None:
         self.datacollector.collect(self)
         self.schedule.step()
 
-
-def agent_portrayal(agent):
-    portrayal = {"Shape": "circle",
-                 "Filled": "true",
-                 "r": 0.5}
-
-    if agent.wealth > 0:
-        portrayal["Color"] = "red"
-        portrayal["Layer"] = 0
-    else:
-        portrayal["Color"] = "grey"
-        portrayal["Layer"] = 1
-        portrayal["r"] = 0.2
-    return portrayal
-
-
-grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
-
-chart = ChartModule([{"Label": "Gini",
-                      "Color": "Black"}],
-                    data_collector_name='datacollector')
-
-server = ModularServer(MoneyModel,
-                       [grid, chart],
-                       "Money Model",
-                       {
-                           "n": 100,
-                           "width": 10,
-                           "height": 10
-                       })
-server.port = 8521  # The default
-server.launch()
+    def run_model(self, n) -> None:
+        for i in range(n):
+            self.step()
